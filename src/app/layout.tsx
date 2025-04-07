992585965d53a05715a5f68d13d17ebc17315a21
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-04-07 16:58:35 +0400

    setup: Format everything using prettier for the first time

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+      <body className="flex min-h-screen flex-col">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
