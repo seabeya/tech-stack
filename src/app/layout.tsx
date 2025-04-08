95033482d7ca149789c6652a43f0905bfbf7191b
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-04-08 21:03:11 +0400

    setup: Add base metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,32 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://tech-stack.seabeya.com'),
+  title: 'Tech Stack',
+  description: 'Personal IT Notes',
+  keywords: ['tech-stack', 'docs', 'documentation', 'IT', 'tutorial', 'open-source', 'seabeya'],
+  openGraph: {
+    type: 'website',
+    url: '/',
+    title: 'Tech Stack',
+    description: 'Personal IT Notes',
+    siteName: 'Tech Stack',
+    images: [
+      {
+        url: '/icons/icon-96x96.png',
+        alt: 'Tech Stack',
+      },
+    ],
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
