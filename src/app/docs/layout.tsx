19f7fdfb4ea80782a8ad9ed8470df2129a447dc1
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-04-10 20:59:00 +0400

    setup: Refactor docs layout

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,12 +1,14 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 
+const docsOptions: DocsLayoutProps = {
+  ...baseOptions,
+  tree: source.pageTree,
+  links: [],
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
-      {children}
-    </DocsLayout>
-  );
+  return <DocsLayout {...docsOptions}>{children}</DocsLayout>;
 }
