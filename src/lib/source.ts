d211d3b898128af64dd9c0275a6bc9b051210380
Sha'an Aliyev <35802638+seabeya@users.noreply.github.com>
2025-04-13 18:37:34 +0400

    core feat: Add page root folder templates

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,9 +1,23 @@
 import { docs } from '@/.source';
+import { DOCS } from '@/consts';
 import { loader } from 'fumadocs-core/source';
+import { createElement } from 'react';
 
 // `loader()` also assign a URL to your pages
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
+  icon(icon) {
+    if (!icon) {
+      return;
+    }
+
+    const doc = DOCS[icon as keyof typeof DOCS];
+    if (doc === undefined) {
+      return;
+    }
+
+    return createElement(doc.icon);
+  },
 });
